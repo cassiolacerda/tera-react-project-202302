b695efcf72b9dabfa236c010750dc28ce3c62a08
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2023-09-12 21:51:30 -0300

    Adiciona navegação

--- src/components/molecules/DrawerMenu.jsx
+++ src/components/molecules/DrawerMenu.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Drawer from "@mui/material/Drawer";
+import { Link } from "react-router-dom";
+import { List, ListItem } from "@mui/material";
 
 export default function DrawerMenu(props) {
   return (
@@ -8,7 +10,17 @@ export default function DrawerMenu(props) {
       open={props.open}
       onClose={() => props.setOpen(false)}
     >
-      Olá mundo!
+      <List>
+        <ListItem>
+          <Link to="/">Home</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/users">Usuários</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/users/1/post">Criar post</Link>
+        </ListItem>
+      </List>
     </Drawer>
   );
 }
