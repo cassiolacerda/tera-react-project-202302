af9f0740eb3653bfc73a517d5181a380e3bda1c9
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2023-09-19 21:32:21 -0300

    Adiciona tela de usuários - parte 2

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,10 +1,8 @@
 /* eslint jsx-a11y/anchor-is-valid:0 */
 import React, { useEffect, useState } from "react";
-import Default from "../templates/Default";
 
-import user1 from "../../images/placeholders/user-1.jpg";
-import user2 from "../../images/placeholders/user-2.jpg";
-import user3 from "../../images/placeholders/user-3.jpg";
+import Default from "../templates/Default";
+import UserListWrapper from "../molecules/UserListWrapper";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
@@ -23,27 +21,7 @@ export default function Users() {
     <Default>
       <div className="users">
         <h1>Users</h1>
-
-        <div className="users__list">
-          <a href="#" className="users__list-item">
-            <div className="users__list-item-photo">
-              <img src={user1} className="responsive avatar" alt="" />
-            </div>
-            <div className="users__list-item-name">John Walter</div>
-          </a>
-          <a href="#" className="users__list-item">
-            <div className="users__list-item-photo">
-              <img src={user2} className="responsive avatar" alt="" />
-            </div>
-            <div className="users__list-item-name">Chris Hudson</div>
-          </a>
-          <a href="#" className="users__list-item">
-            <div className="users__list-item-photo">
-              <img src={user3} className="responsive avatar" alt="" />
-            </div>
-            <div className="users__list-item-name">Bryan Cornell</div>
-          </a>
-        </div>
+        <UserListWrapper users={users} />
       </div>
     </Default>
   );
